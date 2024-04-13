790a13595bd5f69b4e626f5a9fa8e1b7483ee908
Philipp Muens <philipp@muens.io>
2024-04-13 10:08:23 +0200

    Implement Lagrange interpolation

---
+++ src/lagrange-interpolation/main.test.ts
@@ -0,0 +1,42 @@
+import { expect } from "$std/expect/mod.ts";
+import { describe, it } from "$std/testing/bdd.ts";
+
+import { interpolate } from "./main.ts";
+
+describe("Lagrange Interpolation", () => {
+  it("should generate an interpolation polynomial of degree 0", () => {
+    const evaluations = [{ x: 1, y: 2 }];
+
+    const f = interpolate(evaluations);
+
+    const e1 = evaluations[0];
+
+    expect(f(e1.x)).toBe(e1.y);
+  });
+
+  it("should generate an interpolation polynomial of degree 1", () => {
+    const evaluations = [{ x: 1, y: 2 }, { x: 3, y: 4 }];
+
+    const f = interpolate(evaluations);
+
+    const e1 = evaluations[0];
+    const e2 = evaluations[1];
+
+    expect(f(e1.x)).toBe(e1.y);
+    expect(f(e2.x)).toBe(e2.y);
+  });
+
+  it("should generate an interpolation polynomial of degree 2", () => {
+    const evaluations = [{ x: 1, y: 2 }, { x: 2, y: 3 }, { x: 3, y: 6 }];
+
+    const f = interpolate(evaluations);
+
+    const e1 = evaluations[0];
+    const e2 = evaluations[1];
+    const e3 = evaluations[2];
+
+    expect(f(e1.x)).toBe(e1.y);
+    expect(f(e2.x)).toBe(e2.y);
+    expect(f(e3.x)).toBe(e3.y);
+  });
+});
---
+++ src/lagrange-interpolation/main.ts
@@ -0,0 +1,31 @@
+export function interpolate(evaluations: Evaluation[]) {
+  function f(x: number) {
+    let interpolationPolynomial = 0;
+
+    for (let i = 0; i < evaluations.length; i++) {
+      const xi = evaluations[i].x;
+      const yi = evaluations[i].y;
+
+      // Calculate basis polynomial.
+      let basisPolynomial = 1;
+      for (let j = 0; j < evaluations.length; j++) {
+        if (j != i) {
+          const xj = evaluations[j].x;
+          basisPolynomial *= (x - xj) / (xi - xj);
+        }
+      }
+
+      // Rescale basis polynomial.
+      basisPolynomial = yi * basisPolynomial;
+
+      // Add basis polynomial to interpolation polynomial.
+      interpolationPolynomial += basisPolynomial;
+    }
+
+    return interpolationPolynomial;
+  }
+
+  return f;
+}
+
+type Evaluation = { x: number; y: number };
