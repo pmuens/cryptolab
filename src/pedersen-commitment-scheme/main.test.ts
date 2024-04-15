b52ede4d5e48213f5f6a936b78f6fdc0821d8eb1
Philipp Muens <philipp@muens.io>
2024-04-15 11:11:39 +0200

    Remove superfluous await operators

--- src/pedersen-commitment-scheme/main.test.ts
+++ src/pedersen-commitment-scheme/main.test.ts
@@ -6,23 +6,23 @@ import { Secp256k1 } from "../ecc/curve.ts";
 import { createCommitment, verifyCommitment } from "./main.ts";
 
 describe("Pedersen Commitment Scheme", () => {
-  it("should verify a valid commitment", async () => {
+  it("should verify a valid commitment", () => {
     const v = 42n;
-    const { c, r } = await createCommitment(v);
+    const { c, r } = createCommitment(v);
 
-    const isValid = await verifyCommitment(v, r, c);
+    const isValid = verifyCommitment(v, r, c);
 
     expect(isValid).toBe(true);
   });
 
-  it("shouldn't verify an invalid commitment", async () => {
+  it("shouldn't verify an invalid commitment", () => {
     const v = 42n;
 
     const curve = new Secp256k1();
     const c = new Point(curve, 1n, 1n);
 
-    const { r } = await createCommitment(v);
-    const isValid = await verifyCommitment(v, r, c);
+    const { r } = createCommitment(v);
+    const isValid = verifyCommitment(v, r, c);
 
     expect(isValid).toBe(false);
   });
